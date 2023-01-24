ed41287fd364cc4f9377ca1624f791f78f602945
Mohammad Sumon <sumon3011@gmail.com>
2023-01-24 14:01:10 +0600

    implement react hook form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,61 +1,78 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div className='mx-8 lg:mx-0'>
-            <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-            <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-purple-600 lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-purple-700 underline uppercase decoration-solid">
-                   Log In
-                </h1>
-                <form className="mt-6">
-                    <div className="mb-2">
-                        <label
-                            for="email"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            className="required block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
-                    </div>
-                    <div className="mb-2">
-                        <label
-                            for="password"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            className="required block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
-                    </div>
-                    <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                            Login
-                        </button>
-                    </div>
-                </form>
-
-                <p className="mt-8 text-xs font-light text-center text-gray-700">
-                    {" "}
-                    Don't have an account?{" "}
-                    <Link
-                    to="/signup"
-                        href="#"
-                        className="font-medium text-purple-600 hover:underline"
-                    >
-                        Sign up
-                    </Link>
-                </p>
+  const { register, formState: {errors}, handleSubmit } = useForm();
+//   const [data, setData] = useState("");
+const handleLogin = data => {
+    console.log(data);
+    // alert('Login was successful');
+}
+  return (
+    <div className="mx-8 lg:mx-0">
+      <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+        <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-purple-600 lg:max-w-xl">
+          <h1 className="text-3xl font-semibold text-center text-purple-700 underline uppercase decoration-solid">
+            Log In
+          </h1>
+          <form
+            onSubmit={handleSubmit(handleLogin)}
+            className="mt-6"
+          >
+            <div className="mb-2">
+              <label
+                for="email"
+                className="block text-sm font-semibold text-gray-800"
+              >
+                Email
+              </label>
+              <input
+                {...register("email", { required: "Email Address is required" })}
+                placeholder="Email"
+                type="email"
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+              {errors.email && <p className="text-red-600">{errors.email?.message}</p>}
             </div>
+            <div className="mb-2">
+              <label
+                for="password"
+                className="block text-sm font-semibold text-gray-800"
+              >
+                Password
+              </label>
+              <input
+                {...register("password", { required: "Password is required" })}
+                placeholder="Password"
+                type="password"
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+              {errors.password && <p className="text-red-600">{errors.password?.message}</p>}
+            </div>
+            <div className="mt-6">
+              <input
+                type="submit"
+                className="btn btn-outline w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+              />
+            </div>
+          </form>
+
+          <p className="mt-8 text-xs font-light text-center text-gray-700">
+            {" "}
+            Don't have an account?{" "}
+            <Link
+              to="/signup"
+              href="#"
+              className="font-medium text-purple-600 hover:underline"
+            >
+              Sign up
+            </Link>
+          </p>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
